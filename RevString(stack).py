e0180252e895d1d039c3b1aba872551c19dc64d1
Karthikeyen K <114602409+karthikeyen78@users.noreply.github.com>
2023-12-24 23:47:37 +0530

    Update RevString(stack).py

--- RevString(stack).py
+++ RevString(stack).py
@@ -1,1 +1,18 @@
-
+def reverse_stack(str):
+  # Create an empty stack (list to use as stack)
+  stack = []
+   # Push all characters of string to stack
+  for char in str:
+      stack.append(char)
+  # Pop all characters of string
+  # and add it to the rev
+  rev = ''
+  while len(stack) > 0:
+       last = stack.pop()
+       rev = rev + last
+       # print(last, rev)
+       
+  return rev
+usr_str = input('What is your string:')
+reverse = reverse_stack(usr_str)
+print('Reversed is: ', reverse)
