d1b6b9380073ed1391340233cdebbee38db14687
Karthikeyen K <114602409+karthikeyen78@users.noreply.github.com>
2023-12-27 23:54:43 +0530

    Update Cube.py

--- Cube.py
+++ Cube.py
@@ -1,1 +1,11 @@
 #With a given integral number n, write a program to calculate the sum of cubes.
+def cube_sum(num):
+	sum = 0
+	for n in range(num+1):
+		sum = sum + n**3
+	return sum
+
+
+user_num = int(input('Enter a number: '))
+result = cube_sum(user_num)
+print('Your sum of cubes are: ', result)
