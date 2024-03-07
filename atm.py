fa61fd90408eeb201315e2ac82fc9e0b7c06e7a1
Karthikeyen K <114602409+karthikeyen78@users.noreply.github.com>
2024-03-07 23:58:00 +0530

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,1 +1,15 @@
+# cook your dish here
+x, bal = [float(x) for x in input().split()]
 
+res = bal
+
+if x + 0.5 <= bal:
+    if x%5==0:
+        res = bal - ( x + 0.5)
+    
+    
+        
+print("%.2f"%res)
+        
+    
+    
