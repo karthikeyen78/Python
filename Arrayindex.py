09d0662caa3803d43e108aaf1b13a53c1210d358
Karthikeyen K <114602409+karthikeyen78@users.noreply.github.com>
2024-03-28 23:50:58 +0530

    Create Arrayindex.py

---
+++ Arrayindex.py
@@ -0,0 +1,11 @@
+import array as arr    
+a = arr.array('i', [2, 4, 5, 6])    
+print("First element is:", a[0])    
+print("Second element is:", a[1])   
+print("Third element is:", a[2])  
+print("Forth element is:", a[3])  
+print("last element is:", a[-1])    
+print("Second last element is:", a[-2])    
+print("Third last element is:", a[-3])    
+print("Forth last element is:", a[-4])    
+print(a[0], a[1], a[2], a[3], a[-1],a[-2],a[-3],a[-4])  
